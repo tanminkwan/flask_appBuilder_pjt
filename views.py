99716167953e3f4b2a2ef0d275a78fb27e442c53
tanminkwan <tanminkwan@gmail.com>
2021-03-24 04:26:51 +0900

    Add files via upload

---
+++ views.py
@@ -0,0 +1,85 @@
+from flask import render_template
+from flask_appbuilder.models.sqla.interface import SQLAInterface
+from flask_appbuilder import ModelView, ModelRestApi
+from .models import ContactGroup, Contact
+from . import appbuilder, db
+
+"""
+    Create your Model based REST API::
+
+    class MyModelApi(ModelRestApi):
+        datamodel = SQLAInterface(MyModel)
+
+    appbuilder.add_api(MyModelApi)
+
+
+    Create your Views::
+
+
+    class MyModelView(ModelView):
+        datamodel = SQLAInterface(MyModel)
+
+
+    Next, register your Views::
+
+
+    appbuilder.add_view(
+        MyModelView,
+        "My View",
+        icon="fa-folder-open-o",
+        category="My Category",
+        category_icon='fa-envelope'
+    )
+"""
+
+"""
+    Application wide 404 error handler
+"""
+class ContactModelView(ModelView):
+    datamodel = SQLAInterface(Contact)
+
+    label_columns = {'contact_group':'Contacts Group'}
+    list_columns = ['name','personal_cellphone','gender', 'birthday','contact_group']
+
+    show_fieldsets = [
+        (
+            'Summary',
+            {'fields': ['name', 'address', 'contact_group']}
+        ),
+        (
+            'Personal Info',
+            {'fields': ['birthday', 'personal_phone', 'personal_cellphone'], 'expanded': False}
+        ),
+    ]
+
+class GroupModelView(ModelView):
+    datamodel = SQLAInterface(ContactGroup)
+
+    add_columns = ['id', 'name']
+    related_views = [ContactModelView]
+
+
+@appbuilder.app.errorhandler(404)
+def page_not_found(e):
+    return (
+        render_template(
+            "404.html", base_template=appbuilder.base_template, appbuilder=appbuilder
+        ),
+        404,
+    )
+
+
+db.create_all()
+appbuilder.add_view(
+    GroupModelView,
+    "List Groups",
+    icon = "fa-folder-open-o",
+    category = "Contacts",
+    category_icon = "fa-envelope"
+)
+appbuilder.add_view(
+    ContactModelView,
+    "List Contacts",
+    icon = "fa-envelope",
+    category = "Contacts"
+)
